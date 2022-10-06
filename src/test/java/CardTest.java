7a63d2cf579eb44837e359701f4dcbcc8a6e7f1e
YuliaPtitca <107066957+YuliaPtitca@users.noreply.github.com>
2022-10-06 21:11:18 +0300

    Update CardTest.java

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -23,6 +23,7 @@ public class CardTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
