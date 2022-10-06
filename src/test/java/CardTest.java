ce87b1b40808253404dbbf033dee1c3bf421a793
YuliaPtitca <qa.yulia.ptitca@yandex.ru>
2022-10-06 20:30:53 +0300

    initial commit

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,45 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+public class CardTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void shouldTestFullPositiveForm() {
+        driver.get("http://localhost:9999");
+        List<WebElement> inputs = driver.findElements(By.tagName("input"));
+        inputs.get(0).sendKeys("Смирнова Екатерина Петровна");
+        inputs.get(1).sendKeys("+79758868855");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText().trim();
+        Assertions.assertEquals(expected, actual);
+    }
+}
