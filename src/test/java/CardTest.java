44d50c21d21c0f361b0df1bb5452f4f5afbb7330
YuliaPtitca <107066957+YuliaPtitca@users.noreply.github.com>
2022-10-11 20:27:33 +0300

    Update CardTest.java

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Condition.exactText;
 import static java.awt.SystemColor.text;
 
 public class CardTest {
@@ -12,11 +13,10 @@ public class CardTest {
     @Test
     void shouldTestFullPositiveForm() {
         Selenide.open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Смирнова Екатерина");
+        $("[data-test-id=name] input").setValue("Смирнова-Иванова Екатерина");
         $("[data-test-id=phone] input").setValue("+79758868855");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $("[data-test-id=order-success]").shouldHave(text(" Ваша заявка успешно отправлена!" +
-                " Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
